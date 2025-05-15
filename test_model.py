11b3440b0903fc85068f779699f8105727076df6
mzulliev <mzulliev@gmail.com>
2025-05-15 10:29:13 +0300

    changes in README

--- test_model.py
+++ test_model.py
@@ -6,21 +6,23 @@ model = joblib.load("models/heart_disease_model.pkl")
 preprocessor = joblib.load("models/preprocessor.pkl")
 
 sample = pd.DataFrame([{
-    "Age": 70,
+    "Age": 39,
     "Sex": "M",
-    "ChestPainType": "ATA",
-    "RestingBP": 160,
-    "Cholesterol": 320,
-    "FastingBS": 1,
-    "RestingECG": "LVH",
-    "MaxHR": 100,
-    "ExerciseAngina": "Y",
-    "Oldpeak": 3.0,
-    "ST_Slope": "Flat"
+    "ChestPainType": "ASY",
+    "RestingBP": 130,
+    "Cholesterol": 200,
+    "FastingBS": 0,
+    "RestingECG": "Normal",
+    "MaxHR": 190,
+    "ExerciseAngina": "N",
+    "Oldpeak": 0.0,
+    "ST_Slope": "Up"
 }])
 
 X_transformed = preprocessor.transform(sample)
 
 prediction = model.predict(X_transformed)
+predction_proba = model.predict_proba(X_transformed)
 
-print(f'Предсказание (0 = нет болезни, 1 = болезнь): {prediction[0]}')
+print(f'Предсказание (0 = нет болезни, 1 = болезнь): {prediction[0]}\n\
+      Вероятность: {predction_proba[0][prediction[0]]}')
